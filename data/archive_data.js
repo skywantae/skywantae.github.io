c862c48ac8b80f80158fd5e03883b7cc08f63dc3
ReleaseBot <release@kostat.com>
2026-09-17 10:39:20 +0900

    Deploy KOSTAT ERP Mobile WebApp v1.0.167

--- data/archive_data.js
+++ data/archive_data.js
@@ -13,7 +13,7 @@ window.KOSTAT_ARCHIVE_DATA = [
     "tags": [
       "AI"
     ],
-    "download_count": 2,
+    "download_count": 3,
     "is_pinned": false,
     "created_at": "2026-09-16T06:06:21.749Z"
   }
